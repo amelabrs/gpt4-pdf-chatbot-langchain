8ac74532939c95720295cdc94b781dbb9721a828
amelabrs <43107172+amelabrs@users.noreply.github.com>
2023-07-13 16:22:19 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   }>({
     messages: [
       {
-        message: 'Hi, what would you like to learn about Cisco Enterprise Products',
+        message: 'Hi, what would you like to learn about Cisco's Enterprise Products',
         type: 'apiMessage',
       },
     ],
